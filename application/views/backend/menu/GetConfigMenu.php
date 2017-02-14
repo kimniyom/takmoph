c777643283b6bb8866006fc2310428d37fe5c365
kimniyom <kimniyomclub@gmail.com>
2017-02-14 09:43:07 +0700

    2017-02-14

--- application/views/backend/menu/GetConfigMenu.php
+++
@@ -1,7 +0,0 @@
-
-<select id="link" class="form-control">
-    <option value="">เลือกเมนู</option>
-    <?php foreach ($masmenu->result() as $rs): ?>
-        <option value="<?php echo $rs->admin_menu_id; ?>"><?php echo $rs->admin_menu_name; ?></option>
-    <?php endforeach; ?>
-</select>
--- application/views/user/lock.php
+++
@@ -1,4 +0,0 @@
-<center>
-    <img src="http://icons.iconarchive.com/icons/webalys/kameleon.pics/512/Hacker-icon.png"/><br/>
-    <h1 style=" color: #ff0000;">Who are you ... ?</h1>
-</center>
